1f5ffee38ee0f41cd6d68acb7a5dcfb16c6798c4
Parsa K <parsa.kp.2004@gmail.com>
2024-03-20 14:51:30 -0400

    Added an announcements page

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('announcements/', views.announcements, name='announcements'),
 ]
--- main/views.py
+++ main/views.py
@@ -6,6 +6,28 @@ def home(request):
 
 def about(request):
     context = {
-        'title': "About Page"
+        'title': 'About Page'
     }
     return render(request, 'main/about.html', context)
+
+def announcements(request):
+    context = {
+        'announcements': [
+            {
+                'header': 'Header 1', 
+                'title': 'Announcement 1', 
+                'content': 'This is the first announcement'
+            },
+            {
+                'header': 'Header 2', 
+                'title': 'Announcement 2', 
+                'content': 'This is the second announcement'
+            },
+            {
+                'header': 'Header 3', 
+                'title': 'Announcement 3', 
+                'content': 'This is the third announcement'
+            },
+        ]
+    }
+    return render(request, 'main/announcements.html', context)
