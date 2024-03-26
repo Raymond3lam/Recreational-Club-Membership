27ce1c704b1fe72317c27a0822c9fbc6fa5d23b5
Christopher <chrisplacey@hotmail.ca>
2024-03-25 21:50:22 -0400

    login signup html

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('announcements/', views.announcements, name='announcements'),
-    path('login/', views.login, name='login')
+    path('login/', views.user_login, name='login'),
+    path('register/', views.signup, name='signup'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- main/views.py
+++ main/views.py
@@ -13,7 +13,7 @@ def about(request):
     }
     return render(request, 'main/about.html', context)
 
-def register(request):
+def signup(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
@@ -21,21 +21,25 @@ def register(request):
             return redirect('login')
     else:
         form = RegisterForm()
-    return render(request, 'main/register.html', {'form': form})
+    return render(request, 'main/signup.html', {'form': form})
 
-def login(request):
+def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
-            user = authenticate(request, username=username, password=password)
+            user = authenticate(request,username=username, password=password)
             if user:
                 login(request, user)
                 return redirect('home')
     else:
         form = LoginForm()
     return render(request, 'main/login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')
 
 def announcements(request):
     context = {
