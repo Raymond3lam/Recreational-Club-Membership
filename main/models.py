dcff4869d8ff36352f33331cea17072e7356129b
Christopher <chrisplacey@hotmail.ca>
2024-04-09 17:30:09 -0400

    add member and practices

--- main/models.py
+++ main/models.py
@@ -1,6 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Coach(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+class Member(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    practices = models.ManyToManyField('Practice') 
+    def __str__(self):
+        return self.user.username
+
+
+class Practice(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    coach = models.ForeignKey(Coach, on_delete=models.CASCADE)
+    members = models.ManyToManyField(Member)
+    
+    def __str__(self):
+        return self.name
+    
 # Create your models here.
 class Announcement(models.Model):
     title = models.CharField(max_length=100)
