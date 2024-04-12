eb5a08b030fa858c6bff613664a68e943f9fe0d6
ChrisLolz <chrisplacey@hotmail.ca>
2024-04-11 23:03:13 -0400

    manage coachs and practices

--- main/admin.py
+++ main/admin.py
@@ -8,15 +8,21 @@ class PracticeInline(admin.TabularInline):
 class CustomUserAdmin(UserAdmin):
     model = models.CustomUser
     fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('phone_number', 'address', 'paid')}),
+        (None, {'fields': ('phone_number', 'address')}),
     )
     inlines = [PracticeInline]
 
 class PracticeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'coach', 'date')
     filter_horizontal = ('members',)
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'amount', 'date', 'practice')
+    list_filter = ('user', 'date')
+    search_fields = ('user__username', 'practice__name')
 
 # Register your models here.
 admin.site.register(models.Announcement)
 admin.site.register(models.CustomUser, CustomUserAdmin)
 admin.site.register(models.Practice, PracticeAdmin)
-admin.site.register(models.Payment)
+admin.site.register(models.Payment, PaymentAdmin)
